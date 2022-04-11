8906ab9f445bf1f07a1d80ce9cd33eff2d9f51ef
rollepude <rolle.pudas@gmail.com>
2022-04-11 19:19:57 +0300

    .

--- example game/Assets/Lattianousu.cs
+++ example game/Assets/Lattianousu.cs
@@ -6,12 +6,14 @@ public class Lattianousu : MonoBehaviour
 {
 	[SerializeField] private Animator MyAnimatorController;
 	[SerializeField] private Animator MyanimatorController;
+	[SerializeField] private Animator Myanimatorcontroller;
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.CompareTag("Player"))
 		{
 			MyAnimatorController.SetBool("Lattianousu", true);
 			MyanimatorController.SetBool("Piikkinousu", true);
+			Myanimatorcontroller.SetBool("seinänousu", true);
 		}
 	}
 
@@ -22,6 +24,7 @@ public class Lattianousu : MonoBehaviour
         {
             MyAnimatorController.SetBool("Lattianousu", false);
             MyanimatorController.SetBool("Piikkinousu", false);
-        }
+			Myanimatorcontroller.SetBool("seinänousu", false);
+		}
     }
 }
