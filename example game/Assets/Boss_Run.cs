ab6f8b8251f242f9a54a3f526747af04d54b9d01
rollepude <rolle.pudas@gmail.com>
2022-05-25 09:39:01 +0300

    .

--- example game/Assets/Boss_Run.cs
+++ example game/Assets/Boss_Run.cs
@@ -30,11 +30,11 @@ public class Boss_Run : StateMachineBehaviour
 		Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
 		rb.MovePosition(newPos);
 
-        if (Vector2.Distance(player.position, rb.position) <= attackRange)
-        {
-            animator.SetTrigger("Attack");
-        }
-    }
+		if (Vector2.Distance(player.position, rb.position) <= attackRange)
+		{
+			animator.SetTrigger("Attack");
+		}
+	}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
